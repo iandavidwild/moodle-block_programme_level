b4dc61fcdc70650481c554cda41d647f2e153636
Ian Wild <i.wild@ulcc.ac.uk>
2012-10-26 17:53:11 +0100

    Need to be able to click on a node label to expand the tree (not just the '+' icon).

--- module.js
+++ module.js
@@ -29,15 +29,10 @@ M.block_programme_level.init_tree = function(Y, expand_all, htmlid) {
     Y.use('yui2-treeview', function(Y) {
         var tree = new YAHOO.widget.TreeView(htmlid);
 
-        tree.subscribe("clickEvent", function(node, event) {
-            // we want normal clicking which redirects to url
-            return false;
-        });
-
         if (expand_all) {
             tree.expandAll();
         }
 
-        tree.render();
+        tree.draw();
     });
 };
